2b0802d8bd55dec655fe6dab96a876da785f5304
M3LLOUU <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-11 19:20:53 -0300

    Varíaveis

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Jaime";
+let idade = 20;
+let cidade = "Pomerode - SC";
+let altura = 1.77;
+let ehHumano = true;
+
+console.log("Meu nome é", nome);
+console.log("Tenho", idade, "anos");
+console.log("Moro em", cidade);
+console.log("Minha altura é", altura, "m");
+console.log("É humano?", ehHumano);
