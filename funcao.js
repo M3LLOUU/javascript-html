fd26afd70bde277135dd2285fda311b181ed94b4
M3LLOUU <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-15 20:54:47 -0300

    Update script references and add basic arithmetic functions in JavaScript

---
+++ funcao.js
@@ -0,0 +1,28 @@
+/*
+    Função em Portugol
+
+    função real calculadora (real n1, real n2){
+        retorne (n1 + n2) / 2
+        }
+      
+*/ 
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+function multiplicacao(n1, n2) {
+    return n1 * n2
+}
+
+function divisao(n1, n2) {
+    return n1 / n2
+}
+
+function subtracao(n1, n2) {
+    return n1 - n2
+}
+
+let resultado = soma(5, 9);
+
+console.log(`A média é: ${resultado}`);
